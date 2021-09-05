c9babf1b7e5dae3eb0614a13bf662d0139131c3d
Federico <fedeiarlori@hotmail.com>
2021-09-05 16:08:09 -0300

    detalles

--- src/moduloPrincipal/Main.java
+++ src/moduloPrincipal/Main.java
@@ -10,42 +10,44 @@ import manejadorDeArchivo.*;
 public class Main {
     public static void main(String[] args){
 
-        //TODO: controlar la posibilidad de que no hayan suministrado ningun archivo.
-        String filename = args[0];
+        try{
+            String filename = args[0];
 
-
-        boolean sinErrores = true;
-        boolean errorReciente = false;
-        
-        try {
-            AnalizadorLexico analizadorLexico = new AnalizadorLexico(new GestorDeArchivo(filename));
-            Token token = null;
-            do{
-                try{
-                    if(errorReciente){
-                        errorReciente = false;
-                        token = analizadorLexico.proximoTokenDespuesDeError(); //TODO: preguntar si esto esta bien.
-                    } else{
-                        token = analizadorLexico.proximoToken();
-                    }
-                    System.out.println("("+token.getTipoDeToken().toString()+", "+token.getLexema()+", "+token.getNroLinea()+")");
-                } catch (IOException e){
-                    e.printStackTrace();
-                } catch (ExcepcionLexica e){
-                    sinErrores = false;
-                    errorReciente = true;
-                    System.out.println(e.getMessage());
-                }
-            } while(token == null || token.getTipoDeToken() != TipoDeToken.EOF); //TODO: preg si esta muy mal esa condicion ya que null da true solo la primera vez en determinados casos y nunca mas
+            boolean sinErrores = true;
+            boolean errorReciente = false;
             
-            if(sinErrores){
-                System.out.println("\n[SinErrores]");
-            }
+            try {
+                AnalizadorLexico analizadorLexico = new AnalizadorLexico(new GestorDeArchivo(filename));
+                Token token = null;
+                do{
+                    try{
+                        if(errorReciente){
+                            errorReciente = false;
+                            token = analizadorLexico.proximoTokenDespuesDeError(); //TODO: preguntar si esto esta bien.
+                        } else{
+                            token = analizadorLexico.proximoToken();
+                        }
+                        System.out.println("("+token.getTipoDeToken().toString()+", "+token.getLexema()+", "+token.getNroLinea()+")");
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    } catch (ExcepcionLexica e){
+                        sinErrores = false;
+                        errorReciente = true;
+                        System.out.println(e.getMessage());
+                    }
+                } while(token == null || token.getTipoDeToken() != TipoDeToken.EOF); //TODO: preg si esta muy mal esa condicion ya que null da true solo la primera vez en determinados casos y nunca mas
+                
+                if(sinErrores){
+                    System.out.println("\n[SinErrores]");
+                }
 
-        } catch (FileNotFoundException e){
-            System.out.println("no se encontró el archivo para abrir");
-        } catch (IOException e){
-            e.printStackTrace();
-        } 
+            } catch (FileNotFoundException e){
+                System.out.println("Error: no se encontró el archivo en la ruta especificada");
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Error: falta el argumento con la ruta del archivo");
+        }
     }
 }
